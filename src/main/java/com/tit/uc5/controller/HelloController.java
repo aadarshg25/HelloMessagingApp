41c2a7b242b3858185f1ece57c09b5897733b3ef
aadarsh <aadarshg2562003@example.com>
2025-02-22 08:50:13 +0530

    [Aadarsh] Add . uc-5 features added

---
+++ src/main/java/com/tit/uc5/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.tit.uc5.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
+    }
+}
